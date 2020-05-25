55323cc4bf3c2adf4a9ca201958c46058c2ea507
Sherlock <miguel.saaruiz@gmail.com>
2020-05-25 11:37:07 -0500

    corrected import error with opencv

--- vis_tools.py
+++ vis_tools.py
@@ -187,12 +187,12 @@ def visualize_camera_movement(image1, image1_points, image2, image2_points, is_s
         # Coordinates of the same point on t+1 frame
         p2 = (int(image2_points[i][0]), int(image2_points[i][1]))
 
-        cv.circle(image1, p1, 5, (0, 255, 0), 1)
-        cv.arrowedLine(image1, p1, p2, (0, 255, 0), 1)
-        cv.circle(image1, p2, 5, (255, 0, 0), 1)
+        cv2.circle(image1, p1, 5, (0, 255, 0), 1)
+        cv2.arrowedLine(image1, p1, p2, (0, 255, 0), 1)
+        cv2.circle(image1, p2, 5, (255, 0, 0), 1)
 
         if is_show_img_after_move:
-            cv.circle(image2, p2, 5, (255, 0, 0), 1)
+            cv2.circle(image2, p2, 5, (255, 0, 0), 1)
     
     if is_show_img_after_move: 
         return image2
