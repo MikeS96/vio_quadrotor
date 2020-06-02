170950085384397f1f57eb59b0fb19013557b5e5
Sherlock <miguel.saaruiz@gmail.com>
2020-06-02 14:24:35 -0500

    Added euler angle plots and comparison with ground truth

--- vis_tools.py
+++ vis_tools.py
@@ -13,7 +13,7 @@ import cv2
 
 from rotations import Quaternion, skew_symmetric
 
-def visualize_trajectory(trajectory):
+def visualize_trajectory(trajectory, title = "Vehicle's trajectory"):
 
     #list for x, y and z values
     locX = list(trajectory[0,:])
@@ -46,8 +46,8 @@ def visualize_trajectory(trajectory):
     
     ZX_plt.set_title("Trajectory (X, Z)", y=toffset)
     ZX_plt.plot(locX, locZ, "--", label="Trajectory", zorder=1, linewidth=1.5, markersize=2)
-    ZX_plt.set_xlabel("X")
-    ZX_plt.set_ylabel("Z")
+    ZX_plt.set_xlabel("X [m]")
+    ZX_plt.set_ylabel("Z [m]")
     # Plot camera initial location
     ZX_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
     ZX_plt.scatter(locX[-1], locZ[-1], s=8, c="red", label="End location", zorder=2)
@@ -57,7 +57,7 @@ def visualize_trajectory(trajectory):
         
     # Plot ZY
     ZY_plt.set_title("Trajectory (Y, Z)", y=toffset)
-    ZY_plt.set_xlabel("Y")
+    ZY_plt.set_xlabel("Y [m]")
     ZY_plt.plot(locY, locZ, "--", linewidth=1.5, markersize=2, zorder=1)
     ZY_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
     ZY_plt.scatter(locY[-1], locZ[-1], s=8, c="red", label="End location", zorder=2)
@@ -66,8 +66,8 @@ def visualize_trajectory(trajectory):
     
     # Plot YX
     YX_plt.set_title("Trajectory (Y X)", y=toffset)
-    YX_plt.set_ylabel("X")
-    YX_plt.set_xlabel("Y")
+    YX_plt.set_ylabel("X [m]")
+    YX_plt.set_xlabel("Y [m]")
     YX_plt.plot(locY, locX, "--", linewidth=1.5, markersize=2, zorder=1)
     YX_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
     YX_plt.scatter(locY[-1], locX[-1], s=8, c="red", label="End location", zorder=2)
@@ -83,17 +83,17 @@ def visualize_trajectory(trajectory):
     D3_plt.set_ylim3d(minY, maxY)
     D3_plt.set_zlim3d(minZ, maxZ)
     D3_plt.tick_params(direction='out', pad=-2)
-    D3_plt.set_xlabel("X", labelpad=0)
-    D3_plt.set_ylabel("Y", labelpad=0)
-    D3_plt.set_zlabel("Z", labelpad=-5)
+    D3_plt.set_xlabel("X [m]", labelpad=0)
+    D3_plt.set_ylabel("Y [m]", labelpad=0)
+    D3_plt.set_zlabel("Z [m]", labelpad=-5)
     
     # Plotting the result
-    fig.suptitle("Vehicle's trajectory", fontsize=16, y = 1.05)
+    fig.suptitle(title, fontsize=16, y = 1.05)
     D3_plt.view_init(35, azim=45)
     plt.tight_layout()
     plt.show()
 
-def visualize_angles(rotations):
+def visualize_angles(rotations, title = "Vehicle's euler angles"):
 
     #list to unpack roll, pitch, yaw angles
     roll = []
@@ -103,8 +103,7 @@ def visualize_angles(rotations):
     
     for i in range(0, rotations.shape[1]):
         current_rot = rotations[:, i]
-        q = Quaternion(w = current_rot[3], x = current_rot[0], 
-                       y = current_rot[1], z = current_rot[2]).to_euler()
+        q = Quaternion(*current_rot).to_euler()
         
         roll.append(q.item(0))
         pitch.append(q.item(1))
@@ -149,7 +148,7 @@ def visualize_angles(rotations):
     P_plt.set_title(r'Pitch angle $\beta$', y=toffset)
     P_plt.set_ylabel("Roll [rads]")
     P_plt.axes.xaxis.set_ticklabels([])
-    P_plt.plot(x_axis, pitch, "-", linewidth=1.5, label="Trajectory", markersize=2, zorder=1)
+    P_plt.plot(x_axis, pitch, "-", linewidth=1.5, label="Angle", markersize=2, zorder=1)
     P_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
     P_plt.scatter(x_axis[-1], pitch[-1], s=8, c="red", label="End location", zorder=2)
     P_plt.set_xlim([minX, maxX])
@@ -157,9 +156,9 @@ def visualize_angles(rotations):
     
     # Yaw Trajectory
     Y_plt.set_title(r'Yaw angle $\gamma$', y=toffset)
-    Y_plt.set_ylabel("Yaw")
+    Y_plt.set_ylabel("Yaw [rads]")
     Y_plt.axes.xaxis.set_ticklabels([])
-    Y_plt.plot(x_axis, yaw, "-", linewidth=1.5, label="Trajectory", markersize=2, zorder=1)
+    Y_plt.plot(x_axis, yaw, "-", linewidth=1.5, label="Angle", markersize=2, zorder=1)
     Y_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
     Y_plt.scatter(x_axis[-1], yaw[-1], s=8, c="red", label="End location", zorder=2)
     Y_plt.set_xlim([minX, maxX])
@@ -167,7 +166,7 @@ def visualize_angles(rotations):
     Y_plt.legend(loc=4, title="Legend", borderaxespad=0., fontsize="medium", frameon=True)
     
     # Plotting the result
-    fig.suptitle("Vehicle's euler angles", fontsize=16, y = 1.05)
+    fig.suptitle(title, fontsize=16, y = 1.05)
     plt.tight_layout()
     plt.show()
 
@@ -192,3 +191,250 @@ def visualize_camera_movement(image1, image1_points, image2, image2_points, is_s
         return image2
     else:
         return image1
+
+def compare_3d(ground_truth, trajectory, title):
+
+	# Axis limits
+	maxX = np.amax(trajectory[0,:]) + 1
+	minX = np.amin(trajectory[0,:]) - 1
+	maxY = np.amax(trajectory[1,:]) + 1
+	minY = np.amin(trajectory[1,:]) - 1 
+	maxZ = np.amax(trajectory[2,:]) + 1
+	minZ = np.amin(trajectory[2,:]) - 1
+
+	est_traj_fig = plt.figure()
+	ax = est_traj_fig.add_subplot(111, projection='3d')
+	ax.plot(ground_truth[0,:], ground_truth[1,:], ground_truth[2,:], "-", label='Ground Truth')
+	ax.plot(trajectory[0,:], trajectory[1,:], trajectory[2,:], "-", label='Estimated')
+	ax.set_xlabel('X [m]')
+	ax.set_ylabel('Y [m]')
+	ax.set_zlabel('Z [m]')
+	ax.set_title(title)
+	ax.legend()
+	ax.set_xlim(minX, maxX)
+	ax.set_ylim(minY, maxY)
+	ax.set_zlim(minZ, maxZ)
+	plt.tight_layout()
+	plt.show()
+
+def compare_2d_trajectory(ground_truth, trajectory, title = "VO vs Ground Truth Trajectory"):
+
+    #list for x, y and z values
+    locX = list(trajectory[0,:])
+    locY = list(trajectory[1,:])
+    locZ = list(trajectory[2,:])
+    locX_gt = list(ground_truth[0,:])
+    locY_gt = list(ground_truth[1,:])
+    locZ_gt = list(ground_truth[2,:])
+    
+    # Axis limits
+    maxX = np.amax(locX) + 1
+    minX = np.amin(locX) - 1
+    maxY = np.amax(locY) + 1
+    minY = np.amin(locY) - 1 
+    maxZ = np.amax(locZ) + 1
+    minZ = np.amin(locZ) - 1
+
+    # Set styles
+    mpl.rc("figure", facecolor="white")
+    plt.style.use("seaborn-whitegrid")
+
+    # Plot the figure
+    fig = plt.figure(figsize=(8, 6), dpi=100)
+    gspec = gridspec.GridSpec(2, 2)
+    ZY_plt = plt.subplot(gspec[1,1])
+    YX_plt = plt.subplot(gspec[0,1])
+    ZX_plt = plt.subplot(gspec[1,0])
+    D3_plt = plt.subplot(gspec[0,0], projection='3d')
+    
+    toffset = 1.0
+    
+    # Actual trajectory plotting ZX
+    
+    ZX_plt.set_title("Trajectory (X, Z)", y=toffset)
+    ZX_plt.plot(locX, locZ, "--", label="Trajectory GT", zorder=1, linewidth=1.5, markersize=2)
+    ZX_plt.plot(locX_gt, locZ_gt, "--", label="Trajectory Estimated", zorder=1, linewidth=1.5, markersize=2)
+    ZX_plt.set_xlabel("X [m]")
+    ZX_plt.set_ylabel("Z [m]")
+    # Plot camera initial location
+    ZX_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
+    ZX_plt.scatter(locX[-1], locZ[-1], s=8, c="red", label="End location Estimation", zorder=2)
+    ZX_plt.scatter(locX_gt[-1], locZ_gt[-1], s=8, c="purple", label="End location GT", zorder=2)
+    ZX_plt.set_xlim([minX, maxX])
+    ZX_plt.set_ylim([minZ, maxZ])
+    ZX_plt.legend(bbox_to_anchor=(1.05, 1.0), loc=3, title="Legend", borderaxespad=0., fontsize="medium", frameon=True)
+        
+    # Plot ZY
+    ZY_plt.set_title("Trajectory (Y, Z)", y=toffset)
+    ZY_plt.set_xlabel("Y [m]")
+    ZY_plt.plot(locY, locZ, "--", linewidth=1.5, markersize=2, zorder=1)
+    ZY_plt.plot(locY_gt, locZ_gt, "--", linewidth=1.5, markersize=2, zorder=1)
+    ZY_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
+    ZY_plt.scatter(locY[-1], locZ[-1], s=8, c="red", label="End location Estimation", zorder=2)
+    ZY_plt.scatter(locY_gt[-1], locZ_gt[-1], s=8, c="purple", label="End location GT", zorder=2)
+    ZY_plt.set_xlim([minY, maxY])
+    ZY_plt.set_ylim([minZ, maxZ])
+    
+    # Plot YX
+    YX_plt.set_title("Trajectory (Y X)", y=toffset)
+    YX_plt.set_ylabel("X [m]")
+    YX_plt.set_xlabel("Y [m]")
+    YX_plt.plot(locY, locX, "--", linewidth=1.5, markersize=2, zorder=1)
+    YX_plt.plot(locY_gt, locX_gt, "--", linewidth=1.5, markersize=2, zorder=1)
+    YX_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
+    YX_plt.scatter(locY[-1], locX[-1], s=8, c="red", label="End location Estimation", zorder=2)
+    YX_plt.scatter(locY_gt[-1], locX_gt[-1], s=8, c="purple", label="End location GT", zorder=2)
+    YX_plt.set_xlim([minY, maxY])
+    YX_plt.set_ylim([minX, maxX])
+
+    # Plot 3D
+    D3_plt.set_title("3D trajectory", y = 1.1)
+    D3_plt.plot3D(xs = locX, ys = locY, zs = locZ, zorder=0)
+    D3_plt.plot3D(xs = locX_gt, ys = locY_gt, zs = locZ_gt, zorder=0)
+    D3_plt.scatter(0, 0, 0, s=8, c="green", zorder=1)
+    D3_plt.scatter(locX[-1], locY[-1], locZ[-1], s=8, c="red", zorder=1)
+    D3_plt.scatter(locX_gt[-1], locY_gt[-1], locZ_gt[-1], s=8, c="purple", zorder=1)
+    D3_plt.set_xlim3d(minX, maxX)
+    D3_plt.set_ylim3d(minY, maxY)
+    D3_plt.set_zlim3d(minZ, maxZ)
+    D3_plt.tick_params(direction='out', pad=-2)
+    D3_plt.set_xlabel("X [m]", labelpad=0)
+    D3_plt.set_ylabel("Y [m]", labelpad=0)
+    D3_plt.set_zlabel("Z [m]", labelpad=-5)
+    
+    # Plotting the result
+    fig.suptitle(title, fontsize=16, y = 1.05)
+    D3_plt.view_init(35, azim=45)
+    plt.tight_layout()
+    plt.show()
+
+def compare_2d_angles(ground_truth, rotations, title = "VO vs Ground Truth angles"):
+
+    # list to unpack roll, pitch, yaw angles from the ground truth
+    roll = []
+    pitch = []
+    yaw = []
+    # Unpack roll pitch and yaw estimations
+    roll_es = []
+    pitch_es = []
+    yaw_es = []
+    x_axis = np.arange(ground_truth.shape[1])
+    x_axis_es = np.arange(rotations.shape[1])
+    
+    for i in range(0, ground_truth.shape[1]):
+        current_rot = ground_truth[:, i]
+        q = Quaternion(*current_rot).to_euler()
+        
+        roll.append(q.item(0))
+        pitch.append(q.item(1))
+        yaw.append(q.item(2))
+
+    for i in range(0, rotations.shape[1]):
+        current_rot = rotations[:, i]
+        q = Quaternion(*current_rot).to_euler()
+        
+        roll_es.append(q.item(0))
+        pitch_es.append(q.item(1))
+        yaw_es.append(q.item(2))
+        
+    # Axis limits for Roll, Pitch and Yaw Ground Truth
+    maxR = np.amax(roll) + 0.1
+    minR = np.amin(roll) - 0.1
+    maxP = np.amax(pitch) + 0.1
+    minP = np.amin(pitch) - 0.1
+    maxY = np.amax(yaw) + 0.1
+    minY = np.amin(yaw) - 0.1
+    maxX = ground_truth.shape[1] + 10
+    minX = -5
+
+    # Axis limits for Roll, Pitch and Yaw estimated
+    maxR_es = np.amax(roll_es) + 0.1
+    minR_es = np.amin(roll_es) - 0.1
+    maxP_es = np.amax(pitch_es) + 0.1
+    minP_es = np.amin(pitch_es) - 0.1
+    maxY_es = np.amax(yaw_es) + 0.1
+    minY_es = np.amin(yaw_es) - 0.1
+    maxX_es = rotations.shape[1] + 10
+    minX_es = -5
+
+    # Set styles
+    mpl.rc("figure", facecolor="white")
+    plt.style.use("seaborn-whitegrid")
+
+    # Plot the figure
+    fig = plt.figure(figsize=(8, 6), dpi=100)
+    gspec = gridspec.GridSpec(3, 2)
+    R_plt = plt.subplot(gspec[0,0])
+    P_plt = plt.subplot(gspec[1,0])
+    Y_plt = plt.subplot(gspec[2,0])
+    R_plt_es = plt.subplot(gspec[0,1])
+    P_plt_es = plt.subplot(gspec[1,1])
+    Y_plt_es = plt.subplot(gspec[2,1])
+    
+    toffset = 1.0
+    
+    # Roll Trajectory
+    
+    R_plt.set_title(r'Roll angle GT $\theta$', y=toffset)
+    R_plt.plot(x_axis, roll, "-", label="Angle GT", zorder=1, linewidth=1.5, markersize=2)
+    R_plt.set_ylabel("Roll [rads]")
+    # Plot camera initial location
+    R_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
+    R_plt.scatter(x_axis[-1], roll[-1], s=8, c="red", label="End location", zorder=2)
+    R_plt.set_xlim([minX, maxX])
+    R_plt.set_ylim([minR, maxR])
+
+    # Roll Trajectory estimated
+    R_plt_es.set_title(r'Roll angle Estimated $\theta$', y=toffset)
+    R_plt_es.plot(x_axis_es, roll_es, "-", c="orange", label="Angle Estimated", zorder=1, linewidth=1.5, markersize=2)
+    R_plt_es.axes.xaxis.set_ticklabels([])
+    # Plot camera initial location
+    R_plt_es.scatter([0], [0], s=8, c="blue", label="Start location", zorder=2)
+    R_plt_es.scatter(x_axis_es[-1], roll_es[-1], s=8, c="purple", label="End location", zorder=2)
+    R_plt_es.set_xlim([minX_es, maxX_es])
+    R_plt_es.set_ylim([minR_es, maxR_es])
+        
+    # Pitch Trajectory
+    P_plt.set_title(r'Pitch angle GT $\beta$', y=toffset)
+    P_plt.set_ylabel("Roll [rads]")
+    P_plt.axes.xaxis.set_ticklabels([])
+    P_plt.plot(x_axis, pitch, "-", linewidth=1.5, label="Angle GT", markersize=2, zorder=1)
+    P_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
+    P_plt.scatter(x_axis[-1], pitch[-1], s=8, c="red", label="End location", zorder=2)
+    P_plt.set_xlim([minX, maxX])
+    P_plt.set_ylim([minP, maxP])
+
+    # Pitch Trajectory estimated
+    P_plt_es.set_title(r'Pitch angle Estimated $\beta$', y=toffset)
+    P_plt_es.axes.xaxis.set_ticklabels([])
+    P_plt_es.plot(x_axis_es, pitch_es, "-", linewidth=1.5, c="orange", label="Angle Estimated", markersize=2, zorder=1)
+    P_plt_es.scatter([0], [0], s=8, c="blue", label="Start location", zorder=2)
+    P_plt_es.scatter(x_axis_es[-1], pitch_es[-1], s=8, c="purple", label="End location", zorder=2)
+    P_plt_es.set_xlim([minX_es, maxX_es])
+    P_plt_es.set_ylim([minP_es, maxP_es])
+    
+    # Yaw Trajectory
+    Y_plt.set_title(r'Yaw angle GT $\gamma$', y=toffset)
+    Y_plt.set_ylabel("Yaw [rads]")
+    Y_plt.axes.xaxis.set_ticklabels([])
+    Y_plt.plot(x_axis, yaw, "-", linewidth=1.5, label="Angle GT", markersize=2, zorder=1)
+    Y_plt.scatter([0], [0], s=8, c="green", label="Start location", zorder=2)
+    Y_plt.scatter(x_axis[-1], yaw[-1], s=8, c="red", label="End location", zorder=2)
+    Y_plt.set_xlim([minX, maxX])
+    Y_plt.set_ylim([minY, maxY])
+    Y_plt.legend(loc=4, title="Legend", borderaxespad=0., fontsize="medium", frameon=True)
+
+    # Yaw Trajectory estimated
+    Y_plt_es.set_title(r'Yaw angle Estimated $\gamma$', y=toffset)
+    Y_plt_es.axes.xaxis.set_ticklabels([])
+    Y_plt_es.plot(x_axis_es, yaw_es, "-", linewidth=1.5, c="orange", label="Angle Estimated", markersize=2, zorder=1)
+    Y_plt_es.scatter([0], [0], s=8, c="blue", label="Start location", zorder=2)
+    Y_plt_es.scatter(x_axis_es[-1], yaw_es[-1], s=8, c="purple", label="End location", zorder=2)
+    Y_plt_es.set_xlim([minX_es, maxX_es])
+    Y_plt_es.set_ylim([minY_es, maxY_es])
+    Y_plt_es.legend(loc=4, title="Legend", borderaxespad=0., fontsize="medium", frameon=True)
+    
+    # Plotting the result
+    fig.suptitle(title, fontsize=16, y = 1.05)
+    plt.tight_layout()
+    plt.show()
