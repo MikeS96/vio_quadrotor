cb3b2e1c07f78268b215971a997e560ff60e9326
Sherlock <miguel.saaruiz@gmail.com>
2020-06-03 09:46:51 -0500

    Documented Dataset handler

--- dataset_handler.py
+++ dataset_handler.py
@@ -1,4 +1,4 @@
-# Utitlity file with functions for handling trajectory plots
+# Class to handle images
 #
 # Author: Miguel Saavedra
 
@@ -12,9 +12,18 @@ from operator import itemgetter
 class DatasetHandler:
 
     def __init__(self, img_path, depth_path, imu_df):
+        """
+        Class to handle Grayscale, RGB and depth maps in a comprehensive way with lists.
+
+        :param img_path: Images path
+        :param depth_path: Depth maps path
+        :param imu_df: IMU dataframe
+        """
+
         # Define number of frames
         self.num_frames = 0
         self.depth_frames = 0
+
         # Define reference dataframe to sync timestamps
         self.imu = imu_df
 
@@ -29,25 +38,29 @@ class DatasetHandler:
         self.depth_maps = []
         self.time_list = []
 
+        # Camera intrinsic
         self.k = np.array([[410, 0, 640],
                            [0, 410, 360],
                            [0,   0,   1]], dtype=np.float32)
 
-        
         # Read first frame
         self.read_frame()
         
     def read_frame(self):
+        """ Read Image and Depth maps """
         self._read_image()
         self._read_depth()
               
     def _read_image(self):
-        for filename in glob.glob(self.image_dir + '*.jpeg'): #assuming gif
+        """ Read RGB and Grayscale images with their timestamp and store
+        them in two different lists where images[0] return a tuple. images[0][0] 
+        is the current timestamp and images[0][1] is the image. """
+        for filename in glob.glob(self.image_dir + '*.jpeg'): 
             base_name = os.path.basename(filename)
             # Split the time stamp of each image and save it in a list
-            # nsecs is divided 1000000
+            # nsecs is divided 1000000000
             current_time = float(base_name.rsplit("secs_",2)[1].split('n')[0]) + float(base_name.rsplit("nsecs_",1)[1].split('.')[0])/1000000000
-            # Inverted bits value, image is being wrongly readed
+            # Save Grayscale and RGB images in a list with their timestamp
             self.images.append([current_time, cv2.imread(filename, 0)])
             self.images_rgb.append([current_time, cv2.imread(filename)[:, :, ::-1]])
             self.time_list.append(current_time)
@@ -56,6 +69,7 @@ class DatasetHandler:
 
 
     def _sort_timestamp(self):
+        """ Sort the images and depth maps timestamps """
         # Sorting grayscale images
         self.images = sorted(self.images, key = itemgetter(0))
         # Sorting rgb images
@@ -70,44 +84,39 @@ class DatasetHandler:
         self.images = [[np.round(t - min_time, 2), img] for t, img in self.images]
         self.images_rgb = [[np.round(t - min_time, 2), img] for t, img in self.images_rgb]
         self.depth_maps = [[np.round(t - min_time, 2), img] for t, img in self.depth_maps]
+        # Sync timestamps with IMU
         self._sync_timestamp()
 
     def _sync_timestamp(self):
+        """ Synchronize the images and depth maps timestamps with the IMU timestamps """
+        # Transform IMU dataframe into a numpy array
         list_of_inertias = self.imu.values.tolist()
         imu_array = np.array(list_of_inertias).T
-        # Find the closest value for each timestamp in the images and replace it
+        # Find the closest timestamp in the IMU array and replace it in the image time-stamp
         for i in range(self.num_frames):
             minArg = np.argmin(np.abs(imu_array[0,:] - self.images[i][0]))
             self.images[i][0] = imu_array[0, minArg]
             self.images_rgb[i][0] = imu_array[0, minArg]
             self.depth_maps[i][0] = imu_array[0, minArg]
 
-        # Use only every 8th element in the list to avoid too much info
-        #values_to_take = np.linspace(0, self.num_frames, num= self.num_frames / 8, endpoint = False, dtype = int)
-        #self.images = [self.images[i] for i in values_to_take]
-        #self.images_rgb = [self.images_rgb[i] for i in values_to_take]
-        #self.num_frames = len(self.images)
-        #print("Data loaded: {0} image frames".format(int(self.num_frames)), end="\r")
 
     def _read_depth(self):
-        for filename in glob.glob(self.depth_dir + '*.dat'): #assuming gif
+        """ Read Depth maps with their timestamp and store
+        them in a list where depth_maps[0] return a tuple. depth_maps[0][0] 
+        is the current timestamp and depth_maps[0][1] is the depth map. """
+        for filename in glob.glob(self.depth_dir + '*.dat'): 
             base_name = os.path.basename(filename)
-            # Split the time stamp of each image and save it in a list
-            # nsecs is divided 1000000
+            # Split the time stamp of each depth map and save it in a list
+            # nsecs is divided 1000000000
             current_time = float(base_name.rsplit("secs_",2)[1].split('n')[0]) + float(base_name.rsplit("nsecs_",1)[1].split('.')[0])/1000000000
-            # Inverted bits value, image is being wrongly readed
+            # Read depth map 
             depth = np.loadtxt(
                 filename,
                 delimiter=' ',
                 dtype=np.float64)
+            # Store depth map in a class' attribute
             self.depth_maps.append([current_time, depth])
             self.depth_frames += 1
         print("Data loaded: {0} depth frames".format(int(self.depth_frames)), end="\r")
         # Sort time stamps
         self._sort_timestamp()
-
-    def _pop_featureless(self, idx):
-        # Remove the element at a specific position (Featureless)
-        self.images = [self.images[i] for i in range(self.num_frames) if i not in idx]
-        self.images_rgb = [self.images_rgb[i] for i in range(self.num_frames) if i not in idx]
-        self.depth_maps = [self.depth_maps[i] for i in range(self.num_frames) if i not in idx]
